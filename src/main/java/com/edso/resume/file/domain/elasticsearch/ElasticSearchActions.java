c07b5855cb86811663b3d3436cf39ee95e1f74e7
Viet-mq <maiquocviets1@gmail.com>
2021-09-20 08:57:56 +0700

    add elasticsearch

--- src/main/java/com/edso/resume/file/domain/elasticsearch/ElasticSearchActions.java
+++ src/main/java/com/edso/resume/file/domain/elasticsearch/ElasticSearchActions.java
@@ -1,5 +1,21 @@
 package com.edso.resume.file.domain.elasticsearch;
 
+import com.edso.resume.file.domain.request.UploadCVRequest;
+import com.edso.resume.file.service.UploadCVService;
+import org.elasticsearch.client.ElasticsearchClient;
+import org.elasticsearch.client.RestClient;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class ElasticSearchActions {
-    // TODO
+    @Autowired
+    UploadCVRequest uploadCVRequest;
+
+    @Autowired
+    UploadCVService uploadCVService;
+
+    public void insertTextIntoElasticsearch(UploadCVRequest uploadCVRequest){
+        uploadCVService.uploadCV(uploadCVRequest);
+
+    }
+
 }
--- src/main/java/com/edso/resume/file/service/UploadCVServiceImpl.java
+++ src/main/java/com/edso/resume/file/service/UploadCVServiceImpl.java
@@ -2,14 +2,38 @@ package com.edso.resume.file.service;
 
 import com.edso.resume.file.domain.request.UploadCVRequest;
 import com.edso.resume.lib.response.BaseResponse;
+import lombok.SneakyThrows;
+import org.apache.pdfbox.cos.COSDocument;
+import org.apache.pdfbox.io.RandomAccessFile;
+import org.apache.pdfbox.pdfparser.PDFParser;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
 
 @Service
 public class UploadCVServiceImpl extends BaseService implements UploadCVService {
 
+    @SneakyThrows
     @Override
     public BaseResponse uploadCV(UploadCVRequest request) {
-        return null;
+        BaseResponse baseResponse = new BaseResponse();
+        File pdf = new File(String.valueOf(request.getFile()));
+        String textParsed;
+
+        PDFParser pdfParser = new PDFParser(new RandomAccessFile(pdf, "r"));
+        pdfParser.parse();
+
+        COSDocument cosDocument = pdfParser.getDocument();
+        PDFTextStripper pdfTextStripper = new PDFTextStripper();
+        PDDocument pdDocument = new PDDocument(cosDocument);
+        textParsed = pdfTextStripper.getText(pdDocument);
+
+        baseResponse.setSuccess(textParsed);
+
+        return baseResponse;
+
     }
 
 }
