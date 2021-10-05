01532060d157e1eac1c77284625d6f70989fe1e6
Viet-mq <maiquocviets1@gmail.com>
2021-10-05 22:33:13 +0700

    add swagger

--- src/main/java/com/edso/resume/file/config/Swagger2Config.java
+++ src/main/java/com/edso/resume/file/config/Swagger2Config.java
@@ -2,26 +2,38 @@ package com.edso.resume.file.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import javax.servlet.ServletContext;
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
 public class Swagger2Config {
     @Bean
-    public Docket api() {
+    public Docket swaggerUploadApi() {
 
-        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+        return new Docket(DocumentationType.SWAGGER_2)
+                .globalOperationParameters(
+                        Collections.singletonList(new ParameterBuilder()
+                                .name("Authorization")
+                                .description("Token")
+                                .modelRef(new ModelRef("string"))
+                                .parameterType("header")
+                                .required(false)
+                                .build()))
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.edso.resume.file.controller"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(new ApiInfoBuilder().version("1.0").title("Upload CV API").build());
 
-        docket.pathMapping("/upload-svc/*");
-
-        return docket;
     }
 }
