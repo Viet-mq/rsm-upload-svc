7a8bac070db839837d0bffc5e33ee3f275fa0ac7
Quan Pham <quanpk@edsolabs.com>
2021-10-18 23:26:33 +0700

    update: - UploadCVServiceImpl

--- src/main/java/com/edso/resume/file/service/UploadCVServiceImpl.java
+++ src/main/java/com/edso/resume/file/service/UploadCVServiceImpl.java
@@ -1,12 +1,19 @@
 package com.edso.resume.file.service;
 
+import com.edso.resume.file.domain.db.MongoDbOnlineSyncActions;
 import com.edso.resume.file.domain.entities.Profile;
 import com.edso.resume.file.domain.repo.CvRepo;
 import com.edso.resume.file.domain.request.UploadCVRequest;
+import com.edso.resume.lib.common.CollectionNameDefs;
 import com.edso.resume.lib.response.BaseResponse;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
 import lombok.SneakyThrows;
+import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -17,6 +24,8 @@ import java.util.Objects;
 
 @Service
 public class UploadCVServiceImpl extends BaseService implements UploadCVService {
+
+    private final MongoDbOnlineSyncActions db;
 
     @Value("${pdf.domain}")
     private String domain;
@@ -30,11 +39,24 @@ public class UploadCVServiceImpl extends BaseService implements UploadCVService 
     @Autowired
     CvRepo cvRepo;
 
+    public UploadCVServiceImpl(MongoDbOnlineSyncActions db) {
+        this.db = db;
+    }
+
     @SneakyThrows
     @Override
     public BaseResponse uploadCV(UploadCVRequest request) {
         String textParsed;
         BaseResponse baseResponse = new BaseResponse();
+
+        Bson cond = Filters.eq("id", request.getProfileId());
+        Document idDocument = db.findOne(CollectionNameDefs.COLL_PROFILE, cond);
+
+        if(idDocument == null){
+            baseResponse.setFailed("Id profile không tồn tại");
+            return baseResponse;
+        }
+
         MultipartFile fileUpload = request.getFile();
         File file = convertToFile(fileUpload);
         String extension = Objects.requireNonNull(fileUpload
@@ -71,6 +93,13 @@ public class UploadCVServiceImpl extends BaseService implements UploadCVService 
             profile1.setUrl(domain + fileUpload.getOriginalFilename());
             cvRepo.saveContent(profile1);
         }
+
+        Bson update = Updates.combine(
+                Updates.set("cv", fileUpload.getOriginalFilename()),
+                Updates.set("cv_type", extension)
+        );
+        db.update(CollectionNameDefs.COLL_PROFILE, cond, update, true);
+
         baseResponse.setSuccess("OK");
         return baseResponse;
 
